893c6a46da4c729ddbd50f8df369c034ff8803c1
Bernard Murphy <bernard.murphy@rutgers.edu>
2020-08-19 19:46:04 -0400

    Did this assignment plus stretch goals

--- index.js
+++ index.js
@@ -39,9 +39,27 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(someFood){
+  if (this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return String(this.name) + ", " + String(this.age); 
+}
+
+
 
 /*
   TASK 2
@@ -57,7 +75,26 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+
+Car.prototype.fill = function(gallons){
+  this.tank += gallons;
+}
+
+Car.prototype.drive = function(distance){
+  if (this.tank > (distance/this.milesPerGallon)){
+    this.odometer += distance;
+    this.tank -= (distance/this.milesPerGallon);
+  } else {
+    this.odometer += (distance - (this.milesPerGallon * ((distance/this.milesPerGallon) - this.tank)));
+    this.tank = 0;
+    return "I ran out of fuel at " + String(this.odometer) + " miles";
+  }
 
 }
 
@@ -68,18 +105,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this);
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.constructor = Person;
+
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Global binding - If "this" is used in the global scope, it's value will be the entire window object.
+  2. Implicit binding - Whenever a function is called using a dot, "this" will refer to whatever is immediately left of the dot.
+  3. New Binding - When a constructor function contains "this", "this" refers only to whatever individual objects are created by that function. 
+  4. Explicit binding - When a "call" or "apply" method is used, "this" is already explicitly defined.
 */
 
 
